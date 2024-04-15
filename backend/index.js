1a3ebb12181386a38000562a6700bda8a3f0f530
AbhiiiDev <abhishek.2125cs1088@kiet.edu>
2024-04-15 23:11:54 +0530

    FEAT: User-route-setuped

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,14 @@
 const express = require("express");
 
+const app=express();
+
+
+const apiRoutes=require('./routes');
+
+
+app.use('/api',apiRoutes);
+
+app.listen(3000,()=>{
+    console.log('Server is listening ');
+});
 
