1faffbc7179e6a9fec95dc8599a1626e2b79ee8b
AbhiiiDev <abhishek.2125cs1088@kiet.edu>
2024-04-17 00:03:08 +0530

    FEAT: OPtimised transaction by adding mongoose transaction

--- backend/controller/account-controller.js
+++ backend/controller/account-controller.js
@@ -1,68 +1,67 @@
 const { Account, User } = require("../db");
+const {mongoose}=require('mongoose');
 
-async function getBalance(req,res)
-{
 
-    const account=await Account.findOne({
-        userId:req.userId
-    })
+async function getBalance(req, res) {
+  const account = await Account.findOne({
+    userId: req.userId,
+  });
 
-    return res.status(200).json({balance:account.balance});
-
+  return res.status(200).json({ balance: account.balance });
 }
 
-async function transferMoney(req,res)
-{
-    const {to,amount}=req.body;
+async function transferMoney(req, res) {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  const { to, amount } = req.body;
 
-    const account=await Account.findOne({
-        userId:req.userId
-    })
+  const account = await Account.findOne({
+    userId: req.userId,
+  }).session(session);
 
-    if(account.balance<amount)
+  if (!account || account.balance < amount) {
+    await session.abortTransaction();
+    return res.status(400).json({
+      message: "Insufficient balance",
+    });
+  }
+
+  const toAccount = await Account.findOne({
+    userId: to,
+  }).session(session);
+
+  if (!toAccount) {
+    await session.abortTransaction();
+    return res.status(400).json({
+      message: "account does not exists",
+    });
+  }
+  await Account.updateOne(
     {
-        return res.status(400).json({
-            message:'Insufficient balance'
-        })
+      userId: req.userId,
+    },
+    {
+      $inc: {
+        balance: -amount,
+      },
     }
-
-    const toAccount=await Account.findOne({
-        userId:to
-    })
-
-    if(!toAccount)
+  ).session(session);
+  await Account.updateOne(
     {
-        return res.status(400).json({
-            message:'account does not exists'
-        })
+      userId: to,
+    },
+    {
+      $inc: {
+        balance: amount,
+      },
     }
-        await Account.updateOne({
-            userId:req.userId,
-
-        },
-    {
-        $inc:{
-            balance: -amount
-        }
-    })
-        await Account.updateOne({
-            userId:to,
-
-        },
-    {
-        $inc:{
-            balance: amount
-        }
-    })
-res.status(200).json({
-    message:'Money transferred successfully'
-})
-
-
-
+  ).session(session);
+  await session.commitTransaction();
+  res.status(200).json({
+    message: "Money transferred successfully",
+  });
 }
-module.exports={
-    getBalance,
-    transferMoney
-    
-}
+module.exports = {
+  getBalance,
+  transferMoney,
+};
