97efc725afa831c2075c04ed5f3f73a87fca871e
AbhiiiDev <abhishek.2125cs1088@kiet.edu>
2024-04-18 03:22:42 +0530

    FEAT: Added get user api call + Bug exists- model not changed according to user

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,6 +1,26 @@
+import axios from 'axios';
 import Input from '../components/Input'
-
+import { useEffect, useState } from 'react';
 const Main = () => {
+
+useEffect(()=>{
+getUser();
+
+},[])
+
+const getUser=async()=>{
+    const response=await axios.get('http://localhost:3000/api/v1/user');
+    console.log(response
+    .data.users);
+
+    setUsers(response.data.users
+    );
+    console.log(users)
+}
+
+const [users,setUsers]=useState([]);
+
+
   return (
     <div className="m-5 p-5">
       <span className="font-bold text-xl">You Balance</span> :{" "}
@@ -12,40 +32,49 @@ const Main = () => {
           className="border-2 border-gray-500 w-1/2 rounded-md p-1"
           placeholder="search users"
         />
+        <div>
+            
+        </div>
       </div>
       <hr />
-      <div className="mt-4 grid grid-flow-col">
+     {
+        users.map((user)=>{
+            return (
+            <div key={user.username} className="mt-4 grid grid-flow-col">
+            <div className="avatar placeholder flex items-center col-span-11">
+              <div className="bg-neutral text-neutral-content rounded-full w-12 ">
+                <span>AY</span>
+              </div>
+              <span className="m-2">{user.firstName+" "+user.lastName}</span>
+            </div>
+            <button className="btn btn-neutral col-span-1" onClick={()=>document.getElementById('my_modal_1').showModal()}>Send Money</button>
+            <dialog id="my_modal_1" className="modal">
+      <div className="modal-box">
+        <h2 className="font-bold text-2xl text-center">Send Money!</h2>
+    
         <div className="avatar placeholder flex items-center col-span-11">
-          <div className="bg-neutral text-neutral-content rounded-full w-12 ">
-            <span>AY</span>
-          </div>
-          <span className="m-2">Abhishek Verma</span>
+              <div className="bg-neutral text-neutral-content rounded-full w-12 ">
+                <span>AY</span>
+              </div>
+              <span className="m-2 font-semibold text-xl">{user.firstName+user.lastName}</span>
+            </div>
+            <p className="font-semibold">Amount (in Rs)</p>
+           <div className='flex flex-col justify-center'>
+            <Input/>
+            <button className='btn mt-4 btn-success w-1/2'>Send</button>
+            </div>
+        <div className="modal-action">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn">Close</button>
+          </form>
         </div>
-        <button className="btn btn-neutral col-span-1" onClick={()=>document.getElementById('my_modal_1').showModal()}>Send Money</button>
-        <dialog id="my_modal_1" className="modal">
-  <div className="modal-box">
-    <h2 className="font-bold text-2xl text-center">Send Money!</h2>
-
-    <div className="avatar placeholder flex items-center col-span-11">
-          <div className="bg-neutral text-neutral-content rounded-full w-12 ">
-            <span>AY</span>
-          </div>
-          <span className="m-2 font-semibold text-xl">Abhishek Verma</span>
-        </div>
-        <p className="font-semibold">Amount (in Rs)</p>
-       <div className='flex flex-col justify-center'>
-        <Input/>
-        <button className='btn mt-4 btn-success w-1/2'>Send</button>
-        </div>
-    <div className="modal-action">
-      <form method="dialog">
-        {/* if there is a button in form, it will close the modal */}
-        <button className="btn">Close</button>
-      </form>
-    </div>
-  </div>
-</dialog>
       </div>
+    </dialog>
+          </div>
+            )
+        })
+     }
     </div>
   );
 };
